891f125b8350b87c2250d0fb67bab07e8b89c215
ubuntu_so <ubuntu_so@ubuntuso.myguest.virtualbox.org>
2024-11-26 11:02:50 +0000

    changed files
    updated functions

--- 2/part1alt.c
+++ 2/part1alt.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/mman.h>
 
 #define BUF_SIZE 64
+
+
+// STRUCTS
 
 typedef struct{
     float* x;
@@ -11,6 +18,9 @@ typedef struct{
     float* z;
 
     int numPts;
+} pointCloud;
+
+typedef struct{
     float minX;
     float maxX;
     float avgX;
@@ -23,56 +33,82 @@ typedef struct{
     float maxZ;
     float avgZ;
     float stdZ;
-} pointCloud;
+} statMetrics;
+
 
 // AUXILIARY FUNCTIONS
-int calculateMetricsPointCloud(pointCloud* pc){
+
+long long int calculateElapsedTime(struct timespec init, struct timespec end){
+    long long int et = 0;
+
+    if((end.tv_nsec - init.tv_nsec) < 0){
+            et = (end.tv_sec - init.tv_sec - 1) + (1000000000 + end.tv_nsec - init.tv_nsec);
+    } else{
+            et = (end.tv_sec - init.tv_sec) + (end.tv_nsec - init.tv_nsec);
+    }
+
+    return et;
+}
+
+int writePointCloud(const char* file, const pointCloud* pc){
+    FILE* pcfile = fopen(file, "w");
+    if(!pcfile){perror ("Error opening file!\n"); return -1;}
+    
+    for(int i = 0; i < pc->numPts; i++){
+        fprintf(pcfile, "%f %f %f\n", pc->x[i], pc->y[i], pc->z[i]);
+    }
+    
+    fclose(pcfile);
+    return 0;
+}
+
+statMetrics* calculateStatMetrics(pointCloud* pc){
+    statMetrics *sm = (statMetrics*)malloc(sizeof(statMetrics));
     float sumX = 0;
     float sumY = 0;
     float sumZ = 0;
 
     for(int i = 0; i < pc->numPts; i++){
-        pc->minX = pc->x[0]; pc->maxX = pc->x[0];
-        pc->minY = pc->y[0]; pc->maxY = pc->y[0];
-        pc->minZ = pc->z[0]; pc->maxZ = pc->z[0];
+        sm->minX = pc->x[0]; sm->maxX = pc->x[0];
+        sm->minY = pc->y[0]; sm->maxY = pc->y[0];
+        sm->minZ = pc->z[0]; sm->maxZ = pc->z[0];
 
-        if(pc->x[i] < pc->minX) pc->minX = pc->x[i];
-        if(pc->x[i] > pc->maxX) pc->maxX = pc->x[i];
+        if(pc->x[i] < sm->minX) sm->minX = pc->x[i];
+        if(pc->x[i] > sm->maxX) sm->maxX = pc->x[i];
         sumX += pc->x[i];
 
-        if(pc->y[i] < pc->minY) pc->minY = pc->y[i];
-        if(pc->y[i] > pc->maxY) pc->maxY = pc->y[i];
+        if(pc->y[i] < sm->minY) sm->minY = pc->y[i];
+        if(pc->y[i] > sm->maxY) sm->maxY = pc->y[i];
         sumY += pc->y[i];
 
-        if(pc->z[i] < pc->minZ) pc->minZ = pc->z[i];
-        if(pc->z[i] > pc->maxZ) pc->maxZ = pc->z[i];
+        if(pc->z[i] < sm->minZ) sm->minZ = pc->z[i];
+        if(pc->z[i] > sm->maxZ) sm->maxZ = pc->z[i];
         sumZ += pc->z[i];
     }
 
-    // calculate statistical metrics
-
-    // mean (average)
-    pc->avgX = sumX/pc->numPts;
-    pc->avgY = sumY/pc->numPts;
-    pc->avgZ = sumZ/pc->numPts;
+    sm->avgX = sumX/pc->numPts;
+    sm->avgY = sumY/pc->numPts;
+    sm->avgZ = sumZ/pc->numPts;
 
     // variance (needed for standard deviation)
     float varX, varY, varZ;
     for(int i = 0; i < pc->numPts; i++){
-        varX += pow(pc->x[i] - pc->avgX, 2)/pc->numPts;
-        varY += pow(pc->y[i] - pc->avgY, 2)/pc->numPts;
-        varZ += pow(pc->z[i] - pc->avgZ, 2)/pc->numPts;
+        varX += pow(pc->x[i] - sm->avgX, 2)/pc->numPts;
+        varY += pow(pc->y[i] - sm->avgY, 2)/pc->numPts;
+        varZ += pow(pc->z[i] - sm->avgZ, 2)/pc->numPts;
     }
 
     // standard deviation
-    pc->stdX = sqrt(varX);
-    pc->stdY = sqrt(varY);
-    pc->stdZ = sqrt(varZ);
+    sm->stdX = sqrt(varX);
+    sm->stdY = sqrt(varY);
+    sm->stdZ = sqrt(varZ);
 
-    return 0;
+    return sm;
 }
 
+
 // ASSIGNMENT FUNCTIONS
+
 pointCloud* readPointCloud(const char* file){
     FILE* pcfile = fopen(file, "r");
     if(!pcfile){perror ("Error opening file!\n"); return NULL;}
@@ -96,7 +132,7 @@ pointCloud* readPointCloud(const char* file){
     pc->numPts = numPts;
 
     // process each line, storing xyz values to pc
-    for(int i = 0; i < numPts; i++){
+    for(int i = 0; i < pc->numPts; i++){
         // check for errors and fscanf each line
         int retVal = 0;
         retVal = fscanf(pcfile, "%f %f %f", &pc->x[i], &pc->y[i], &pc->z[i]);
@@ -109,68 +145,87 @@ pointCloud* readPointCloud(const char* file){
             fclose(pcfile);
             return NULL;
         }
+
     }
 
-    calculateMetricsPointCloud(pc);
     return pc;
 }
 
 int preProcessPointCloud(pointCloud* pc){
     // (a)- remove all points with negative x values
+    int aux_i = 0;
+
     for(int i = 0; i < pc->numPts; i++){
-        if(pc->x[i] < 0){
-            float* tmpX = (float*)malloc(sizeof(float)*(pc->numPts-1));
-            float* tmpY = (float*)malloc(sizeof(float)*(pc->numPts-1));
-            float* tmpZ = (float*)malloc(sizeof(float)*(pc->numPts-1));
-
-            //memmove(tmpX, pc, (i+1)*sizeof(float));
-            //memmove(tmpX+i, (pc)+(i+1), (pc->numPts-i)*sizeof(float));
-
-            //memmove(tmpY, pc, (i+1)*sizeof(float));
-            //memmove(tmpY+i, (pc)+(i+1), (pc->numPts-i)*sizeof(float));
-
-            //memmove(tmpZ, pc, (i+1)*sizeof(float));
-            //memmove(tmpZ+i, (pc)+(i+1), (pc->numPts-i)*sizeof(float));
-
-            pc->x = tmpX;
-            pc->y = tmpY;
-            pc->z = tmpZ;
-
-            pc->numPts--;
-
-            free(tmpX);
-            free(tmpY);
-            free(tmpZ);
+        if(pc->x[i] >= 0){
+            pc->x[aux_i] = pc->x[i];
+            pc->y[aux_i] = pc->y[i];
+            pc->z[aux_i] = pc->z[i];
+            aux_i++;
         }
     }
+
+    pc->numPts = aux_i;
+
+    // reallocate space to remove unutilized points
+    pc->x = (float*)realloc(pc->x, aux_i*sizeof(float));
+    pc->y = (float*)realloc(pc->y, aux_i*sizeof(float));
+    pc->z = (float*)realloc(pc->z, aux_i*sizeof(float));
+
+    // (b)-
 
     return 0;
 }
 
+
+// MAIN
+
 int main(int argc, char* argv[]){
+    // lock paging & set highet priority (RTS)
+    mlockall(MCL_CURRENT | MCL_FUTURE);
+    int pid = getpid();
+    setpriority(PRIO_PROCESS, pid, -20);
+
+    // calculate initial time
+    struct timespec init, end;
+    clock_gettime(CLOCK_MONOTONIC, &init);
+
     pointCloud* pc1 = readPointCloud("support_material/point_cloud1.txt");
     pointCloud* pc2 = readPointCloud("support_material/point_cloud2.txt");
     pointCloud* pc3 = readPointCloud("support_material/point_cloud3.txt");
 
-    //preProcessPointCloud(pc1);
-    //preProcessPointCloud(pc2);
-    //preProcessPointCloud(pc3);
+    statMetrics* sm1 = calculateStatMetrics(pc1);
+    statMetrics* sm2 = calculateStatMetrics(pc2);
+    statMetrics* sm3 = calculateStatMetrics(pc3);
 
     printf("POINT CLOUD 1:\n");
-    printf("X: min = %f, max = %f, average = %f, standard deviation = %f\n", pc1->minX, pc1->maxX, pc1->avgX, pc1->stdX);
-    printf("Y: min = %f, max = %f, average = %f, standard deviation = %f\n", pc1->minY, pc1->maxY, pc1->avgY, pc1->stdY);
-    printf("Z: min = %f, max = %f, average = %f, standard deviation = %f\n", pc1->minZ, pc1->maxZ, pc1->avgZ, pc1->stdZ);
+    printf("X: min = %f, max = %f, average = %f, standard deviation = %f\n", sm1->minX, sm1->maxX, sm1->avgX, sm1->stdX);
+    printf("Y: min = %f, max = %f, average = %f, standard deviation = %f\n", sm1->minY, sm1->maxY, sm1->avgY, sm1->stdY);
+    printf("Z: min = %f, max = %f, average = %f, standard deviation = %f\n", sm1->minZ, sm1->maxZ, sm1->avgZ, sm1->stdZ);
 
     printf("POINT CLOUD 2:\n");
-    printf("X: min = %f, max = %f, average = %f, standard deviation = %f\n", pc2->minX, pc2->maxX, pc2->avgX, pc2->stdX);
-    printf("Y: min = %f, max = %f, average = %f, standard deviation = %f\n", pc2->minY, pc2->maxY, pc2->avgY, pc2->stdY);
-    printf("Z: min = %f, max = %f, average = %f, standard deviation = %f\n", pc2->minZ, pc2->maxZ, pc2->avgZ, pc2->stdZ);
+    printf("X: min = %f, max = %f, average = %f, standard deviation = %f\n", sm2->minX, sm2->maxX, sm2->avgX, sm2->stdX);
+    printf("Y: min = %f, max = %f, average = %f, standard deviation = %f\n", sm2->minY, sm2->maxY, sm2->avgY, sm2->stdY);
+    printf("Z: min = %f, max = %f, average = %f, standard deviation = %f\n", sm2->minZ, sm2->maxZ, sm2->avgZ, sm2->stdZ);
 
     printf("POINT CLOUD 3:\n");
-    printf("X: min = %f, max = %f, average = %f, standard deviation = %f\n", pc3->minX, pc3->maxX, pc3->avgX, pc3->stdX);
-    printf("Y: min = %f, max = %f, average = %f, standard deviation = %f\n", pc3->minY, pc3->maxY, pc3->avgY, pc3->stdY);
-    printf("Z: min = %f, max = %f, average = %f, standard deviation = %f\n", pc3->minZ, pc3->maxZ, pc3->avgZ, pc3->stdZ);
+    printf("X: min = %f, max = %f, average = %f, standard deviation = %f\n", sm3->minX, sm3->maxX, sm3->avgX, sm3->stdX);
+    printf("Y: min = %f, max = %f, average = %f, standard deviation = %f\n", sm3->minY, sm3->maxY, sm3->avgY, sm3->stdY);
+    printf("Z: min = %f, max = %f, average = %f, standard deviation = %f\n", sm3->minZ, sm3->maxZ, sm3->avgZ, sm3->stdZ);
+    
+    preProcessPointCloud(pc1);
+    preProcessPointCloud(pc2);
+    preProcessPointCloud(pc3);
 
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    printf("\nExecution time: %d (ms)\n", (int)(calculateElapsedTime(init, end)/1e6));
+
+
+    writePointCloud("support_material/point_cloud1_preprocessed.txt", pc1);
+    writePointCloud("support_material/point_cloud2_preprocessed.txt", pc2);
+    writePointCloud("support_material/point_cloud3_preprocessed.txt", pc3);
+
+    munlockall();
     return 0;
 }
 
